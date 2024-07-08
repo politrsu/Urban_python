5761f8821a93037e3ce1f176f92caf0c6374c0f8
Dmitry Kuchumov <kuchumov@MacBook-Pro-Dmitry.local>
2024-07-08 22:02:04 +0300

    first

---
+++ homework_1_extra.py
@@ -0,0 +1,11 @@
+marks = [[5,3,3,5,4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+Aaron_average = sum(marks[0]) / len(marks[0])
+Bilbo_average = sum(marks[1]) / len(marks[1])
+Johhny_average = sum(marks[2]) / len(marks[2])
+Khendrik_average = sum(marks[3]) / len(marks[3])
+Steve_average = sum(marks[4]) / len(marks[4])
+print('Aaron:', Aaron_average,
+'Bilbo:', Bilbo_average,
+'Johhny:', Johhny_average,
+'Khendrik:', Khendrik_average,
+'Steve:', Steve_average)
