bbfd7e32813afd3708662ec30e5fedf8b2abc7de
politrsu <kuchumoff@gmail.com>
2024-07-23 19:30:19 +0300

    modul_2_5

--- modul_2_5.py
+++ modul_2_5.py
@@ -1,13 +1,12 @@
 def get_matrix(n, m, value):
-  matrix = []
-  for str in range(n):
-      row = []
-  for stol in range(m):
-      row = [value] * m
-      matrix.append(row)
-  return matrix
+    matrix = []
+    for i in range(n):
+        for j in range(n):
+            matrix.append(m * [value])
+        return matrix
 
-result1 = get_matrix(2, 2, 10)
+
+result1 = get_matrix(4, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
 print(result1)
