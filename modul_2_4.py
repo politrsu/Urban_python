224bc0ffa73988da7199b5dba71c9742f9f03e6b
politrsu <68913708+politrsu@users.noreply.github.com>
2024-07-24 00:17:07 +0300

    Update modul_2_4.py

--- modul_2_4.py
+++ modul_2_4.py
@@ -1,16 +1,15 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for number in numbers:
-    is_prime = True  # число простое
+x = 0
+for number in range(2, len(numbers) + 1):
     for divisor in range(2, number):
-        if number % divisor == 0: # если делитель без остатка
-            is_prime = False  # число не простое
-            break # прерывание цикла
-    if is_prime:
+        if number % divisor == 0:
+            x = x + 1
+    if x == 0:
         primes.append(number)
     else:
+        x = 0
         not_primes.append(number)
-
-print("Primes:", primes)
-print("Not Primes:", not_primes)
+print('primes:', primes)
+print('not_primes:', not_primes)
